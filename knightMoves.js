ee17ba5c39d1d3cba01d3314a891fcdd529a0b5e
Ben-Long50 <benjlong50@gmail.com>
2024-04-29 17:08:03 -0700

    fixed a recursion problem with the knightMoves function output

--- knightMoves.js
+++ knightMoves.js
@@ -15,19 +15,18 @@ export default function knightMoves(
     const knight = new Knight(currentSpace);
     if (knight.aList.find((move) => move[0] === endX && move[1] === endY)) {
       currentPath.push(endSpace);
-      return [movesTaken, currentPath];
+      console.log(
+        `To reach it's final destination, the knight moved ${movesTaken} times!`,
+      );
+      console.log('The path taken was:');
+      currentPath.forEach((space) => {
+        console.log(`[${space}]`);
+      });
+      return;
     }
     for (let j = 0; j < knight.aList.length; j++) {
       queue.push([knight.aList[j], [...currentPath]]);
     }
   }
-  const [moves, path] = knightMoves(queue[0], endSpace, movesTaken, queue);
-
-  console.log(
-    `To reach it's final destination, the knight moved ${moves} times!`,
-  );
-  console.log('The path taken was:');
-  path.forEach((space) => {
-    console.log(`[${space}]`);
-  });
+  knightMoves(queue[0], endSpace, movesTaken, queue);
 }
