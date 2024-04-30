cc1781fe06502e5d2f3b177d7fe5201323771c55
Ben-Long50 <benjlong50@gmail.com>
2024-04-29 17:29:36 -0700

    changed test coordinates

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import knightMoves from './knightMoves.js';
 
 knightMoves([5, 0], [2, 7]);
-knightMoves([1, 1], [2, 2]);
+knightMoves([0, 0], [2, 2]);
 knightMoves([0, 0], [7, 7]);
 knightMoves([4, 2], [7, 3]);
